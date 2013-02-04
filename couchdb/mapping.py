0901c53aa91b2a8b7f6b974561b1b23429454ae5
Ryan Olson <rmolson@gmail.com>
2013-02-03 21:14:57 -0700

    New Schematics based Mapping and updated unittests

---
+++ couchdb/mapping.py
@@ -0,0 +1,167 @@
+import inspect
+from schematics.models import Model
+from schematics.types import *
+from schematics.types.compound import ListType
+
+class Document(Model):
+
+    def __init__(self, id=None, *args, **kwargs):
+        super(Document, self).__init__(*args,**kwargs)
+        if id:
+           self.id = id
+
+    def __repr__(self):
+        return '<%s %r@%r %r>' % (type(self).__name__, self.id, self.rev,
+                                  dict([(k, v) for k, v in self._data.items()
+                                        if k not in ('_id', '_rev')]))
+
+    def _get_id(self):
+        if hasattr(self._data, 'id'): # When data is client.Document
+            return self._data.id
+        return self._data.get('_id')
+    def _set_id(self, value):
+        if self.id is not None:
+            raise AttributeError('id can only be set on new documents')
+        self._data['_id'] = value
+    id = property(_get_id, _set_id, doc='The document ID')
+
+    @property
+    def rev(self):
+        """The document revision.
+        :rtype: basestring
+        """
+        if hasattr(self._data, 'rev'): # When data is client.Document
+            return self._data.rev
+        return self._data.get('_rev')
+
+    def items(self):
+        retval = []
+        if self.id is not None:
+            retval.append(('_id', self.id))
+            if self.rev is not None:
+                retval.append(('_rev', self.rev))
+        for name, value in self._fields.items():
+            if name not in ('_id', '_rev'):
+                retval.append((name,self._data[value.field_name]))
+        return retval
+
+    @classmethod
+    def wrap(cls, data):
+        instance = cls()
+        instance._data = data
+        return instance
+
+    @classmethod
+    def load(cls, db, id):
+        """Load a specific document from the given database.
+        
+        :param db: the `Database` object to retrieve the document from
+        :param id: the document ID
+        :return: the `Document` instance, or `None` if no document with the
+                 given ID was found
+        """
+        doc = db.get(id)
+        if doc is None:
+            return None
+        return cls.wrap(doc)
+
+    def store(self, db):
+        """Store the document in the given database."""
+        db.save(self._data)
+        return self
+
+    @classmethod
+    def query(cls, db, map_fun, reduce_fun, language='javascript', **options):
+        """Execute a CouchDB temporary view and map the result values back to
+        objects of this mapping.
+        
+        Note that by default, any properties of the document that are not
+        included in the values of the view will be treated as if they were
+        missing from the document. If you want to load the full document for
+        every row, set the ``include_docs`` option to ``True``.
+        """
+        return db.query(map_fun, reduce_fun=reduce_fun, language=language,
+                        wrapper=cls._wrap_row, **options)
+
+    @classmethod
+    def view(cls, db, viewname, **options):
+        """Execute a CouchDB named view and map the result values back to
+        objects of this mapping.
+        
+        Note that by default, any properties of the document that are not
+        included in the values of the view will be treated as if they were
+        missing from the document. If you want to load the full document for
+        every row, set the ``include_docs`` option to ``True``.
+        """
+        return db.view(viewname, wrapper=cls._wrap_row, **options)
+
+    @classmethod
+    def _wrap_row(cls, row):
+        doc = row.get('doc')
+        if doc is not None:
+            return cls.wrap(doc)
+        data = row['value']
+        data['_id'] = row['id']
+        return cls.wrap(data)
+
+class BasicField(object):
+    def __init__(self, *args, **kwargs):
+        if 'name' in kwargs:
+           kwargs['field_name'] = kwargs['name']
+           del kwargs['name']
+        keys = sorted(kwargs.keys())
+        for kw in keys:
+           print kw, ":", kwargs[kw]
+        super(BasicField, self).__init__()
+
+    def _to_json(self, value):
+        return self.for_json(value)
+
+    def _to_python(self, value):
+        return self.for_python(value)
+
+class TextField(BasicField,StringType):
+    pass
+
+class FloatField(BasicField,FloatType):
+    pass
+
+class IntegerField(BasicField,IntType):
+    pass
+
+class LongField(BasicField,FloatType):
+    pass
+
+class BooleanField(BasicField,BooleanType):
+    pass
+
+class DecimalField(BasicField,DecimalType):
+    pass 
+
+class DateField(BasicField,DateTimeType):
+    pass
+
+class DateTimeField(BasicField,DateTimeType):
+    pass
+
+class TimeField(BasicField,DateTimeType):
+    pass
+
+class DictField(BasicField,DictType):
+    pass
+
+class ListField(ListType):
+    def __init__(self, field, **kwargs):
+        if 'name' in kwargs:
+           kwargs['field_name'] = kwargs['name']
+           del kwargs['name']
+        keys = sorted(kwargs.keys())
+        for kw in keys:
+           print kw, ":", kwargs[kw]
+        field_instance = field
+        if inspect.isclass(field):
+           if issubclass(field, BaseType):
+              field_instance = field()
+        super(ListField, self).__init__(field_instance, **kwargs)
+        
+
---
+++ couchdb/tests/mapping.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007-2009 Christopher Lenz
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from decimal import Decimal
+import doctest
+import unittest
+import datetime
+
+from couchdb import design, mapping
+from schematics.models import Model
+from schematics.types import *
+from schematics.types.compound import ListType, ModelType
+
+from couchdb.tests import testutil
+
+class DocumentTestCase(testutil.TempDatabaseMixin, unittest.TestCase):
+
+    class Post(mapping.Document):
+        title = StringType()
+
+    def testDictType(self):
+        class Test(mapping.Document):
+            d = DictType()
+        a = Test(id='a')
+        b = Test()
+        a.d['x'] = True
+        b.id = 'b'
+        self.assertTrue(a.d.get('x'))
+        self.assertFalse(b.d.get('x'))
+        a.store(self.db)
+        b.store(self.db)
+        a = Test.load(self.db,'b')
+        b = Test.load(self.db,'a')
+        self.assertTrue(b.d.get('x'))
+        self.assertFalse(a.d.get('x'))
+
+    def testAutomaticID(self):
+        post = self.Post(title='foo bar')
+        assert post.id is None
+        post.store(self.db)
+        assert post.id is not None
+        self.assertEqual('foo bar', self.db[post.id]['title'])
+
+    def testExplicitIDViaInit(self):
+        post = self.Post(id='foo_bar',title='foo bar')
+        self.assertEqual(post.id, 'foo_bar')
+        post.store(self.db)
+        self.assertEqual('foo bar', self.db['foo_bar']['title'])
+        
+    def testExplicitIDViaSetter(self):
+        post = self.Post(title='foo bar')
+        post.id = 'foo_bar'
+        self.assertEqual(post.id, 'foo_bar')
+        post.store(self.db)
+        self.assertEqual('foo bar', self.db['foo_bar']['title'])
+        
+    def testChangeIDFailure(self):
+        post = self.Post(title='foo bar')
+        assert post.id is None
+        post.store(self.db)
+        assert post.id is not None
+        try:
+            post.id = 'new id'
+            self.fail('Expected AttributeError')
+        except AttributeError, e:
+            self.assertEqual('id can only be set on new documents', e.args[0])
+        
+    def testBatchUpdate(self):
+        post1 = self.Post(title='foo bar')
+        post2 = self.Post(title='foo baz')
+        results = self.db.update([post1, post2])
+        self.assertEqual(2, len(results))
+        assert results[0][0] is True
+        assert results[1][0] is True
+
+    def testStoreExisting(self):
+        post = self.Post(title='Foo bar')
+        post.store(self.db)
+        post.store(self.db)
+        self.assertEqual(len(list(self.db.view('_all_docs'))), 1)
+
+
+class ModelTypeTestCase(testutil.TempDatabaseMixin, unittest.TestCase):
+
+    class Post(mapping.Document):
+        class Comment(Model):
+            author = StringType()
+            comment = StringType()
+        title = StringType()
+        comments = ListType(ModelType(Comment))
+
+
+    class User(mapping.Document):
+        class AuthToken(Model):
+            token = UUIDType(auto_fill=True)
+            created_on = DateTimeType(default=datetime.datetime.utcnow)
+        email = EmailType()
+        password = MD5Type()
+        tokens = ListType(ModelType(AuthToken))
+        
+    def testPost(self):
+        post = self.Post(id='foo_bar', title="Foo bar")
+        assert isinstance(post.comments, ListType.Proxy)
+        post.comments.append(self.Post.Comment(author="myself", comment="blah blah blah"))
+        post.store(self.db)
+        self.assertEqual(self.db['foo_bar']['comments'], [{u'author': u'myself', u'comment': u'blah blah blah'}])
+
+    def testTokenCreation(self):
+        authtoken = self.User.AuthToken()
+        assert authtoken.token is not None
+        assert authtoken.created_on is not None
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(doctest.DocTestSuite(mapping))
+    suite.addTest(unittest.makeSuite(DocumentTestCase, 'test'))
+    suite.addTest(unittest.makeSuite(ModelTypeTestCase, 'test'))
+#   suite.addTest(unittest.makeSuite(ListFieldTestCase, 'test'))
+#   suite.addTest(unittest.makeSuite(WrappingTestCase, 'test'))
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
