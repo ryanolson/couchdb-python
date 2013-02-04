c8fb3d9f094fb69f1348eaca9f7abf0bdba077b6
Ryan Olson <rmolson@gmail.com>
2013-02-04 15:20:57 -0700

    updated fixtures

--- couchdb/tests/fixtures.py
+++ couchdb/tests/fixtures.py
@@ -6,6 +6,31 @@ from schematics.types import *
 from schematics.types.compound import ListType, ModelType
 from schematics.serialize import wholelist, whitelist, blacklist
 
+"""
+AuthUser is the base class for a more general User class.  AuthUser
+is designed to encapsulate all the basic authentication for the
+User class.
+
+AuthUser tests ModelType via the AuthUser.Session class.  This ModelType
+tests an auto_fill fields and two fields with default options, one being
+callable and the other being None.
+
+AuthUser requires both the email and _password fields be valid.  AuthUser
+also tests the ListType field using the Session ModelType as entries.
+
+AuthUser also has a "hidden" field in _password.  The password
+property uses the _get_password and _set_password for a getter/setter.
+The setter automatically hashes the value passed with a salted string.
+CouchDB will not let us save a field with the name "_password", 
+(CouchDB reserves _'ed attributes), so we use the "print_name" feature
+so that the value of the _password field will be serialized to "password"
+with any of the schematics.serialize routines.
+
+This leaved one headache.  We don't want to rehash the value of password
+when loading the Document from CouchDB.  Thus we have the condition in 
+__init__ that ignores the "password" keyword if "_rev" is present.
+
+"""
 class AuthUser(Document):
     class Session(Model):
         token = UUIDType(auto_fill=True)
@@ -13,7 +38,7 @@ class AuthUser(Document):
         device_info = DictType(default=None)
         class Options:
            roles = {
-              'mysessions': whitelist('created_on', 'device_info')
+              'mysessions': blacklist('token')
            }
     email = EmailType(required=True)
     sessions = ListType(ModelType(Session))
